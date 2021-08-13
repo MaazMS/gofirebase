46123ba9d55e5547f70f5d3dec586db6bd462b53
MaazMS <maazshaikh7020@gmail.com>
2021-08-13 18:03:52 +0530

    configuration firebase,gin, and database

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"gofirebase/api"
+	"gofirebase/config"
+)
+
+func main() {
+
+	// initialize new gin engine (for server)
+	r := gin.Default()
+	fmt.Println("14")
+	// create/configure database instance
+	db := config.CreateDatabase()
+
+	// set db & firebase auth to gin context with a middleware to all incoming request
+	r.Use(func(c *gin.Context) {
+		c.Set("db", db)
+		c.Set("firebaseAuth", firebaseAuth)
+	})
+
+	// routes definition for finding and creating artists
+	r.GET("/artist", api.FindArtists)
+	r.POST("/artist", api.CreateArtist)
+
+	// start the server
+	r.Run(":5000")
+
+}
